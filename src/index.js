f0ecd521f0de29321318de962f7c9cbd041d5e9e
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-03-09 13:38:15 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+exports.min = function min(array = []) {
+    const reducer = (accumulator, currentValue) =>
+        accumulator < currentValue ? accumulator : currentValue;
+    return array.length > 0 ? array.reduce(reducer) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    const reducer = (accumulator, currentValue) =>
+        accumulator > currentValue ? accumulator : currentValue;
+    return array.length > 0 ? array.reduce(reducer) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = []) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.length > 0 ? array.reduce(reducer) / array.length : 0;
+};
